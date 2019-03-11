bfebfd3d4b4a7d53fbdc583076d07a151b6402b6
TravisBassett <veristis@gmail.com>
2019-03-11 01:42:44 -0700

    some progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WATS3020-finalproject
 Final Project for WATS3020
+
+This project allows the user to input their gender and age and displays a list of Seattle homeless shelters that they may be eligible for. 
--- main.js
+++ main.js
@@ -1,25 +1,32 @@
+// declares universal variables for user's age and gender
+let userAge;
+let userGender;
+
 // genderMChecked see is male radio button is checked and returned a bool value
 // indication if the user is male. If male radio is not checked, user is assumed 
 // to be female.
 function genderMChecked() {
-    var isChecked = document.getElementById("genderm").checked;
+    let isChecked = document.getElementById("genderm").checked;
     return isChecked;
 }
 
-// retries users age
+// retrives users age
 function getUserAge() {
-    console.log('this doesn\'t do anything yet');
+    let foundAge = document.getElementById("ageselector").value;
+    return foundAge;
 }
 
 // the main search funciton, retrieves user gender and age and determines matching shelter
 function searchRequest() {
-    if (genderMChecked()) {
+    // determines users gender as 'm' or 'f' depending on bool values for check of genderm radio button
+    if (genderMChecked() === true) {
         userGender = 'm';
     } else {
         userGender = 'f';
     };
-    let userAge = getUserAge();
-    console.log('search ran');
+    userAge = getUserAge();
+    // for testing
+    console.log(userAge, userGender);
 }
 
 // applies event listener on search button after DOM is fully loaded to avoid console
